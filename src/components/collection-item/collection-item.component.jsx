049e4eeb79a9cd34146f3ce0757e921d06339cca
romansik <roman.sikov@easypark.net>
2022-02-07 12:06:18 +0200

    add items to cart redux and implement adding items in collection-item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { AddItem } from '../../redux/cart/cart-actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+  const {name, price, imageUrl} = item
+  return (
   <div className="collection-item">
     <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
       <span className="name"> {name}</span>
       <span className="price"> {price}</span>
     </div>
-  </div>
-);
+    <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
 
-export default CollectionItem;
+  </div>)
+};
+
+const mapDispatchToProps = dispatch => ( {
+  addItem: item => dispatch(AddItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
