335a1a43f860d7586939af25a19ccf345451bb24
merewolf <shikovi4ik@hotmail.com>
2022-02-09 12:51:08 +0200

    add stripe integration, update favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100; //stripe wants price in cents
+  const publishableKey =
+    'pk_test_51KRCrsFGgOMiAg3VJm8uEJm5ulsOtIurRBMFZHODiKxPP9cMjWDDycZ0XKsMPz7pzUkkY75IssZHDkaEpqWWHd6z00DjYm3Zhi';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Clothing Shop"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
